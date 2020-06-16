550f367d067e95f2cbb965018bb23b7bb69c4678
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2020-06-16 13:36:57 -0700

    bind group impl up to 16

--- ezgfx/src/bind/group.rs
+++ ezgfx/src/bind/group.rs
@@ -126,5 +126,17 @@ macro_rules! impl_bind_group_tuple
 
 impl_bind_group_tuple!({T0}, {0});
 impl_bind_group_tuple!({T0, T1}, {0, 1});
+impl_bind_group_tuple!({T0, T1, T2}, {0, 1, 2});
 impl_bind_group_tuple!({T0, T1, T2, T3}, {0, 1, 2, 3});
 impl_bind_group_tuple!({T0, T1, T2, T3, T4}, {0, 1, 2, 3, 4});
+impl_bind_group_tuple!({T0, T1, T2, T3, T4, T5}, {0, 1, 2, 3, 4, 5});
+impl_bind_group_tuple!({T0, T1, T2, T3, T4, T5, T6}, {0, 1, 2, 3, 4, 5, 6});
+impl_bind_group_tuple!({T0, T1, T2, T3, T4, T5, T6, T7}, {0, 1, 2, 3, 4, 5, 6, 7});
+impl_bind_group_tuple!({T0, T1, T2, T3, T4, T5, T6, T7, T8}, {0, 1, 2, 3, 4, 5, 6, 7, 8});
+impl_bind_group_tuple!({T0, T1, T2, T3, T4, T5, T6, T7, T8, T9}, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9});
+impl_bind_group_tuple!({T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10}, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+impl_bind_group_tuple!({T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11}, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11});
+impl_bind_group_tuple!({T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12}, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12});
+impl_bind_group_tuple!({T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13}, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13});
+impl_bind_group_tuple!({T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14}, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14});
+impl_bind_group_tuple!({T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15}, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15});
