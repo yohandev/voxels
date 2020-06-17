281cdce7300a3b07373eb4c3ae7e67ba14321bb0
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2020-06-17 02:12:44 -0700

    commented code a lil

--- voxels/src/systems/gfx/render.rs
+++ voxels/src/systems/gfx/render.rs
@@ -13,23 +13,34 @@ pub(super) fn system() -> Box<dyn Schedulable>
         // system
         .build(|_, world, res, query|
         {
+            // resources not loaded yet
             if res.is_none()
             {
                 return;
             }
             let res = res.as_ref().unwrap();
 
+            // go through every renderer, though there should
+            // only be one
             for mut ctx in query.iter_mut(world)
             {
+                // output frame
                 let mut frame = ctx.frame();
+
+                // <frame>
                 {
+                    // render pass
                     let mut pass = ctx.render_pass(&mut frame, [0.1, 0.2, 0.3, 1.0]);
 
+                    // material, geometry
                     pass.pipeline(&res.pipeline);
                     pass.geometry(&res.geo);
 
+                    // draw one instance
                     pass.draw(0..1);
                 }
+                // </frame>
+
                 ctx.submit(&mut frame);
             }
         })
