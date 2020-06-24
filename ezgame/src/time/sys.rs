bbaf29309c3017b42edf81a39f4c58c54c5ce4a3
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2020-06-24 15:48:24 -0700

    time system

--- ezgame/src/time/sys.rs
+++ ezgame/src/time/sys.rs
@@ -1,6 +1,12 @@
+use std::time::*;
+
 use crate::ecs::*;
 use crate::evt;
 
+/// system that manages time for your game,
+/// invoking the update and render events
+/// and caching time information in the
+/// RTime resource.
 pub struct STime;
 
 impl System for STime
@@ -10,9 +16,30 @@ impl System for STime
 
     fn exe() -> SysFn
     {
-        sys("ezgame_time_system");
+        // begin...
+        sys("ezgame_time_system")
+        // resources...
+        .read_resource::<crate::window::REvent>()
+        .write_resource::<super::RTime>()
+        .read_resource::<REvents>()
+        // system...
+        .build(|_, _, (r_winit, r_time, r_events), _|
+        {
+            if let winit::event::Event::NewEvents(_) = &**r_winit
+            {
+                let now = Instant::now();
 
-        todo!()
-        //.read_resource()
+                r_time.delta = now.duration_since(r_time.frame);
+                r_time.frame = now;
+            }
+            if let winit::event::Event::MainEventsCleared = &**r_winit
+            {
+                r_events.push(&evt::UPDATE);
+            }
+            if let winit::event::Event::RedrawRequested(_) = &**r_winit
+            {
+                r_events.push(&evt::RENDER);
+            }
+        })
     }
 }
