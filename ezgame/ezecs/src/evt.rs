9949f939056595a0d0ce0d92630d1e0e0afc910e
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2020-06-23 23:12:21 -0700

    events resource and map

--- ezgame/ezecs/src/evt.rs
+++ ezgame/ezecs/src/evt.rs
@@ -1,5 +1,6 @@
 /// event that triggers systems when invoked.
 /// representation is a namespace(`str`) + id(`str`).
+#[derive(Debug, Eq, PartialEq, Hash, Copy, Clone)]
 pub struct Event(&'static str);
 
 /// system execution order, ascending. if system
@@ -20,4 +21,57 @@ pub mod order
     /// system will have low priority and be
     /// amongst the last to be executed.
     pub const LOW: super::Order     = 999;
+}
+
+/// events resource. this is the ezgame provided
+/// event system, which recursively pushes and
+/// polls events. this uses a concurrent queue
+/// internally, so it's recommanded you use a
+/// read_resource() for this, to maximize
+/// parralelism.
+/// ```rust
+/// sys("my_system_a")
+///     .write_resource::<REvents>()
+///     .build(|_, _, events, _|
+///     {
+///         events.push(MyEvent);
+///         // ^^^^ my_system_a blocks the REvents
+///         //      resource, can't run in parallel.
+///     })
+/// sys("my_system_b")
+///     .resource_resource::<REvents>()
+///     .build(|_, _, events, _|
+///     {
+///         events.push(MyEvent);
+///         // ^^^^ my_system_b can run in parallel
+///     })
+/// ```
+#[derive(Debug, Default)]
+pub struct REvents
+{
+    queue: crossbeam::queue::SegQueue<Event>
+}
+
+impl REvents
+{
+    /// push an event to the event queue. note
+    /// that the event won't be immediately
+    /// executed, and duplicates can occur.
+    pub fn push(&self, e: &Event)
+    {
+        self.queue.push(*e);
+    }
+
+    /// internal: pop the events queue for
+    /// processing
+    pub(crate) fn pop(&self) -> Option<Event>
+    {
+        self.queue.pop().ok()
+    }
+
+    /// internal: is the events queue empty?
+    pub(crate) fn is_empty(&self) -> bool
+    {
+        self.queue.is_empty()
+    }
 }
--- ezgame/ezecs/src/map.rs
+++ ezgame/ezecs/src/map.rs
@@ -1,5 +1,46 @@
+use std::collections::HashMap;
+
+use crate::*;
+
 /// maps events to systems schedules.
 pub struct EventSystems
 {
-    
+    map: HashMap<Event, Systems>
+}
+
+impl EventSystems
+{
+    /// recursively process all events inside
+    /// the REvents resource.
+    pub fn process(&mut self, registry: &mut Registry, resources: &mut Resources)
+    {
+        // retrieve events
+        let events = resources
+            .remove::<REvents>()
+            .unwrap();
+        
+        // replace events
+        resources
+            .insert(REvents::default());
+
+        // break recursion
+        if events.is_empty()
+        {
+            return;
+        }
+
+        // process all events
+        while let Some(e) = events.pop()
+        {
+            // get systems
+            if let Some(systems) = self.map.get_mut(&e)
+            {
+                // run systems
+                systems.execute(registry, resources);
+            }
+        }
+        
+        // recurse
+        self.process(registry, resources);
+    }
 }
