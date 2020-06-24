8c91e04f980c41bfd37fa64dc66b469a165f7e5e
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2020-06-23 17:39:26 -0700

    Sys ordering

--- ezgame/ezecs/src/build.rs
+++ ezgame/ezecs/src/build.rs
@@ -13,3 +13,29 @@ struct Sys
     exe: crate::SysFn,
     ord: crate::Order,
 }
+
+impl PartialEq for Sys
+{
+    fn eq(&self, other: &Self) -> bool
+    {
+        self.ord.eq(&other.ord)
+    }
+}
+
+impl PartialOrd for Sys
+{
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering>
+    {
+        self.ord.partial_cmp(&other.ord)
+    }
+}
+
+impl Eq for Sys { }
+
+impl Ord for Sys
+{
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering
+    {
+        self.ord.cmp(&other.ord)
+    }
+}
