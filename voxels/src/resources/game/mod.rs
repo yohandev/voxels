c23f6ac090d0ab42fb83d34ad7b998032fa7f269
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2020-06-29 18:03:26 -0700

    deleted migrating files... they're on origin anyways

--- voxels/src/resources/game/mod.rs
+++
@@ -1,9 +0,0 @@
-mod chunk;
-
-pub use chunk::*;
-
-/// add all the game resources to the application
-pub fn add_game_resources(app: &mut ezgame::Application)
-{
-    app.resources().insert(LoadedChunks::default());
-}
